3d106f409a1bf1315970f98592706d2f7901e816
thummelt <hummelt.tim@googlemail.com>
2021-05-25 11:28:37 +0200

    Adding constants script

---
+++ code/src/modules/constants.py
@@ -0,0 +1,49 @@
+#######################################################################################
+# General                                                                             #
+#######################################################################################
+
+# Time intervals in minutes
+tau: int = 0.25
+
+# Time horizon
+T: int = 25/tau
+
+
+#######################################################################################
+# Vehicle                                                                             #
+#######################################################################################
+
+# Charging/Discharging efficiency
+eta: float = 0.9
+
+# Charging rate in kW/tau*h
+mu: float = 9*tau
+
+# Energy consumption
+ny: float = 0.147
+
+# Battery capacity in kWh
+beta: float = 50
+
+# Max battery level
+beta_max: float = 1*beta
+
+# Min battery level
+beta_min: float = 0.2*beta
+
+# Energy level at end of time horizon
+beta_T: float = 0.8*beta
+
+# Avg driving speed in km/h
+gamma: float = 40
+
+#######################################################################################
+# Other                                                                               #
+#######################################################################################
+
+# Large constant
+phi: int = 9999999
+
+# Penalty costs for not attending trip
+epsilon: float = 100 
+
