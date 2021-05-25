26336801a6a3d9ec4270ba01b191903d9e1905b9
thummelt <hummelt.tim@googlemail.com>
2021-05-25 11:31:05 +0200

    Adding state transition

--- code/src/modules/transition.py
+++ code/src/modules/transition.py
@@ -1,8 +1,10 @@
 from modules.state import State
 from modules.decision import Decision
 from modules.app import App
+from modules import constants as con
 
 import pandas as pd
+import math
 
 ## Represents transition object
 
@@ -30,30 +32,36 @@ class Transition:
         # Calculate transition to to destination state
         self.performTransition()
 
-        # Check if valid destination state is reached
-        if self.checkDestState():
-            pass
-        else:
-            pass
+        # Check if valid destination state is reached - if not set s_d to None
+        if self.checkDestState() == False:
+            self.s_d = None
+
 
 
     def performTransition(self):
+        
         # t
+        t = self.s_s.get_t()+1
 
         # Battery Load
+        b_l = self.s_s.get_B_L() + con.eta*(self.x.get_x_G2V()-self.x.get_x_V2G()) - con.ny*con.gamma*con.tau*self.s_s.getY()
 
         # Time until arrival
+        v_ta = self.s_s.getY()*(self.s_s.get_V_TA()-1) + self.x.get_x_t*math.ceil(self.s_s.get_D()/con.gamma/con.tau)
 
         # Copy exogenous information from exInfo
+        d = self.ex_info.loc[0, "D"].values[0]
+        p_b = self.ex_info.loc[0, "P_B"].values[0]
+        p_s = self.ex_info.loc[0, "P_S"].values[0]
 
-        pass
+        self.s_d = State(t,v_ta,b_l,d,p_b,p_s)
 
     
     def checkDestState(self) -> bool:
         return self.App.getStateByKey(self.s_d.getKey())
 
     def __str__(self) -> str:
-        return "Transition [(%s) -> (%s) -> (%s)]" % ( self.s_s.__str__(), self.x.__str__(), self.s_d.__str__())
+        return "Transition [(%s) -> (%s) & (p=%f | %s) => (%s)]" % ( self.s_s.__str__(), self.x.__str__(), self.p, self.ex_info.iloc[0,:].to_string(),  self.s_d.__str__())
 
 
 
