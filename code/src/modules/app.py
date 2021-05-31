f53e220f9e925f837f9c65b078738ceaba0f4a7a
thummelt <hummelt.tim@googlemail.com>
2021-05-31 17:02:15 +0200

    adding call for vi

--- code/src/modules/app.py
+++ code/src/modules/app.py
@@ -1,9 +1,14 @@
 from src.modules.state import State
 from src.modules.decision import Decision
 from src.modules import generator as g
+from src.modules.solutionAlgorithms import SolutionAlgorithms 
+from src.modules.probabilities import Probabilities
+from src.modules import constants as con
 
 import pandas as pd
 from typing import List, Dict
+from itertools import product, chain
+import numpy as np
 
 
 
@@ -12,6 +17,7 @@ from typing import List, Dict
 class App:
 
     # Variables
+    sol: SolutionAlgorithms
 
     ## States as dataframe to speed up lookup
     dfStates = pd.DataFrame(columns=["key", "t", "B_L", "V_TA", "D", "P_B", "P_S", "terminal", "V_N", "obj"])
@@ -19,6 +25,9 @@ class App:
     decisions : Dict[State, List[Decision]] = {}
 
     def __init__(self):
+        # Solver
+        self.sol = SolutionAlgorithms()
+
         # States
         self.dfStates = g.constructStates()
 
@@ -26,10 +35,34 @@ class App:
         for s in self.dfStates["obj"]:
             self.decisions[s] = g.constructDecisions(s)  
 
-        # Filter out states without transitions          
 
+        # Prune data frame 
+        ## States without decisions
+        ## states with infeasible Transitions          
 
+    
+    def valueIteration(self) -> bool:
+        # Prepare data 
+        
+        p = Probabilities()
+        
+        # Construct data frame for performance [state, decision, t, key, Trip, Length, Price, p]
+        df = pd.DataFrame(chain.from_iterable([list(product([k], v)) for k,v in self.decisions.items()]), columns=["s", "d"])
+        df["t"] = df["s"].apply(lambda x: x.get_t())
+        df["key"] = df["s"].apply(lambda x: x.get_t())
+
+        # Construct ex_info data frame for all ts
+        df_p = pd.DataFrame()
+
+        for t in np.arange(0, con.T, con.tau*60):
+            df_p = df_p.append(p.getProbabilities(t))
+                        
+        df = pd.merge(df,df_p, on=["t"]).copy()
+        
         # Call VI with states, state-decision pairs (transitions are derived automatically just to get successor state)
+        return df # TODO
+        # return self.sol.performStandardVI(df)
+
 
 
 
